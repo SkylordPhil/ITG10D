0d4b153b3a01e3e39e36f35e7c81f24bb631f906
benplayer5 <116455768+benplayer5@users.noreply.github.com>
2023-03-02 20:16:31 +0100

    EnemyController deletet and added to BaseEnemy + Enemy gets destroyed when health = 0

--- Assets/Projekt/Characters/Enemy/BaseEnemy.cs
+++ Assets/Projekt/Characters/Enemy/BaseEnemy.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour, IDamageable
 {
-    public int maxHealth = 100;
+    public float speed = 2f;
+    public int maxHealth = 1;
     public int currentHealth;
+
+    public GameObject Player;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentHealth = maxHealth; 
+        currentHealth = maxHealth;
+        Player = GameManagerController.Instance.getPlayer().gameObject;
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
+        Transform Playerposition = Player.transform;
 
+        Vector3 displacement = Playerposition.position - transform.position;
+        displacement = displacement.normalized;
+
+        if (Vector2.Distance(Playerposition.position, transform.position) > 1.0f)
+        {
+            transform.position += (displacement * speed * Time.deltaTime);
+        }
     }
 
     public void TakeDamage(int damage)
